6ef8c9d12587ae84d2b58137ef1a19fa5b6a4008
ShumAn <Shum-sol@outlook.com>
2025-01-26 19:34:48 +0300

    task4_Сайт администрирования

--- Module_19_Django/task1/admin.py
+++ Module_19_Django/task1/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Buyer,Game
 
 # Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title','cost','size') #Поля для отображения
+    search_fields = ('title',) # Поля для поиска
+    list_filter = ('size','cost') # Поля фильтрации
+    list_per_page = 20 # Кол-во обьектов на странице
+
+    fieldsets = (
+        (None, {
+            'fields': ('title','cost','size') # Базовая секция
+        }),
+        ('Доп инф',{
+            'classes': ('collapse',), # cкрыть секцию по умолчанию
+            'fields' :('age_limited',)
+        }),
+    )
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name','balance','age') #Поля для отображения
+    search_fields = ('name',) # Поля для поиска
+    list_filter = ('balance','age') # Поля фильтрации
+    list_per_page = 30 # Кол-во обьектов на странице
+    readonly_fields =('balance',) # Поля только для чтения
+
+
+
